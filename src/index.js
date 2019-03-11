f54a00447c66c60b435e8871b4c33bca1a06aea1
Phixo <maurice.bartnig@gmail.com>
2019-03-11 14:04:46 +0100

    Cleanup package.json

--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import createStore from './store';
 import { createAsyncAction, createAsyncActionReducers } from './async';
 import { combineReducers, createReducer, reduceWith } from './reducer';
 
-const Reduckx = {
-    combineReducers,
-    createAsyncAction,
-    createAsyncActionReducers,
-    createReducer,
-    createStore,
-    reduceWith,
-};
-
 export {
-    Reduckx as default,
     combineReducers,
     createAsyncAction,
     createAsyncActionReducers,
